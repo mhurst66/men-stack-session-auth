be18167c5a5102c20f46ba5af397abbfe76620a7
mhurst <matthewhurst1791@gmail.com>
2024-11-04 19:55:27 -0500

    auth sess complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,28 @@
 const express = require("express")
+const bcrypt = require("bcrypt")
 const router = express.Router()
 const User = require("../models/user.js")
 
 module.exports = router
 
 // Auth Routes
-// GET the sign up page
+// GET the sign UP page
 router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs")
 })
+// GET the sign IN page
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs")
+})
+// GET sign OUT page
+router.get("/sign-out", (req, res) => {
+    req.session.destroy()
+    res.redirect("/")
+})
+
 // POST the user info to database
 router.post("/sign-up", async (req, res) => {
-    res.send("Form submission successful")
+    // res.send("Form submission successful")
     const userInDatabase = await User.findOne({ username: req.body.username })
     if (userInDatabase) {
         return res.send("Username already taken.")
@@ -19,4 +30,35 @@ router.post("/sign-up", async (req, res) => {
     if (req.body.password !== req.body.confirmPassword) {
         return res.send("Password and Confirm Password must match!")
     }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+    const user = await User.create(req.body)
+    res.send(`Thanks for signing up ${user.username}`)
+})
+
+// POST the sign in info to the database (checks if the user has correct credentials)
+router.post("/sign-in", async (req, res) => {
+    // First, get the user from the database
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.")
+    }
+
+    // There is a user! Time to test their password with bcrypt
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    )
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.")
+    }
+
+    // There is a user AND they had the correct password. Time to make a session!
+    // Avoid storing the password, even in hashed format, in the session
+    // If there is other data you want to save to 'req.session.user', do so here!
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+    }
+    res.redirect("/")
 })
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require Statements
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
@@ -6,6 +7,8 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+
+const session = require('express-session')
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -24,15 +27,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+)
 
 // Routes
 
 // GET HOME PAGE
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        user: req.session.user,
+    })
 })
 // auth Route
-app.use("/auth", authController);
+app.use("/auth", authController)
+// GET the VIP Lounge
+app.get("/vip-lounge", (req, res) => {
+    if (req.session.user) {
+        res.send(`Welcome to the party ${req.session.user.username}!`)
+    } else {
+        res.send("Sorry, no guests allowed.")
+    }
+})
 
   
 
